115bc62000beb77d71cf8b6baada81ab0d9bc320
Sumitha J <jsumitha012@gmail.com>
2026-09-09 15:52:42 +0530

    Time: 1 ms (67.64%) | Memory: 45.1 MB (55.69%) - LeetSync

---
+++ 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int result = Integer.MAX_VALUE;
+        int distance = 0;
+        for(int i = 0; i<nums.length; i++)
+        {
+            if(nums[i] == target)
+            {
+                distance = Math.abs(i-start);
+                result = Math.min(result, distance);
+                
+            }
+        }
+        return result;
+    }
+}
